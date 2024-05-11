b2b420be5522ed1ed92b063075a362d13f001fe4
SylvieSancto <122183650+SylvieSancto@users.noreply.github.com>
2024-05-10 23:46:45 -0400

    Added a way for a certain amount of mistakes to happen between a certain numer of seconds. In this code in 8 second intervals 5 mistakes woudl happen. Need to introduce a way for the user to input this rather than set variables.

--- autoTyper.py
+++ autoTyper.py
@@ -3,7 +3,8 @@ from tkinter import font
 import pyautogui
 import time
 import threading
-
+import string
+import random
 
 class ExpandoText(Text):
     def insert(self, *args, **kwargs):  # This method is responsible for inserting text into the widget whenever the
@@ -67,6 +68,8 @@ def convert():
     intervals = float(14.20 * (wpm ** -1.15))
     updateButton()
     threading.Thread(target=autoTyper, args=(textToType, intervals)).start()
+    time.sleep(3)
+    threading.Thread(target=chooseLetter).start()
 
 
 def updateButton():
@@ -84,7 +87,20 @@ def autoTyper(textToType, intervals):
     time.sleep(3)
     pyautogui.write(textToType, interval=intervals)
     updateButton()
+def chooseLetter(mistakes = 5):
 
+    i = 0
+    while i <= mistakes:
+        listOfLetter = list(string.ascii_lowercase)
+        randomLetter = random.choice(listOfLetter)
+        pyautogui.write(randomLetter)
+        i += 1
+        time.sleep(.7)
+        if i  == mistakes:
+            mistakeEveryXSecond()
+
+def mistakeEveryXSecond():
+    threading.Timer(8, chooseLetter).start()
 
 root, textBox, wpm_entry = create_gui()
 
